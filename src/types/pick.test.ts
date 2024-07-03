e5716267606823c634998423bc547a66635c3648
Robin Kneepkens <robin@skunk.team>
2024-07-03 12:20:11 +0200

    Finished cleaning up

--- src/types/pick.test.ts
+++ src/types/pick.test.ts
@@ -1,219 +1,196 @@
 import { The } from '../interfaces';
-import { createExample, testTypeImpl } from '../testutils';
+import { assignableTo, testTypeImpl } from '../testutils';
+import { checkOneOrMore } from '../utils';
+import { array } from './array';
 import { boolean } from './boolean';
 import { object, partial } from './interface';
-import { number } from './number';
-import { pick, pickProperties, pickPropertiesInfo } from './pick';
-import { string } from './string';
-import './intersection';
 import { intersection } from './intersection';
-import { union } from './union.js';
-import { literal } from './literal.js';
-import { array } from './array.js';
+import { literal } from './literal';
+import { number } from './number';
+import { narrowPickedKeys, pick, validPick } from './pick';
+import { string } from './string';
+import { union } from './union';
 
-type TestType = The<typeof TestType>;
-const TestType = object('TestType', {
+type A = The<typeof A>;
+const A = object('A', {
     a: string,
     b: boolean,
     c: number,
 });
-
 testTypeImpl({
-    name: `Pick<TestType, 'b' | 'c'>`,
-    type: pick(TestType, ['b', 'c']),
+    type: pick('MyCustomName', A, ['a', 'b']),
+    name: 'MyCustomName',
+});
+testTypeImpl({
+    type: pick(A, ['b', 'c']),
+    name: `Pick<A, 'b' | 'c'>`,
     basicType: 'object',
     validValues: [
         { b: true, c: 5 },
         { b: false, c: -2 },
     ],
     invalidValues: [
-        [{ b: 'blah', c: 3 }, `error in [Pick<TestType, 'b' | 'c'>] at <b>: expected a boolean, got a string ("blah")`],
-        [{ c: 3 }, `error in [Pick<TestType, 'b' | 'c'>]: missing property <b> [boolean], got: { c: 3 }`],
+        [{ b: 'hi', c: 3 }, `error in [Pick<A, 'b' | 'c'>] at <b>: expected a boolean, got a string ("hi")`],
+        [{ c: 3 }, `error in [Pick<A, 'b' | 'c'>]: missing property <b> [boolean], got: { c: 3 }`],
     ],
     validConversions: [
         [
-            { a: 'drop', b: true, c: 2 },
+            { a: 42, b: true, c: 2 }, // `a` is actually of the wrong type, but that shouldn't matter.
             { b: true, c: 2 },
         ],
     ],
 });
 
-type PartialTestType = The<typeof PartialTestType>;
-const PartialTestType = partial('PartialTestType', {
+/** No overlap in property names with `A`. */
+type B = The<typeof B>;
+const B = partial('B', {
     d: string,
     e: number,
 });
 testTypeImpl({
-    name: `Pick<PartialTestType, 'd'>`,
-    type: pick(PartialTestType, ['d']),
+    name: `Pick<B, 'd'>`,
+    type: pick(B, ['d']),
     basicType: 'object',
-    validValues: [{ d: 'blah' }, {}],
-    invalidValues: [[{ d: undefined }, `error in [Pick<PartialTestType, 'd'>] at <d>: expected a string, got an undefined`]],
+    validValues: [{ d: 'string' }, {}],
+    invalidValues: [[{ d: undefined }, `error in [Pick<B, 'd'>] at <d>: expected a string, got an undefined`]],
     validConversions: [
-        [{ b: true, c: 3 }, {}],
-        [{ d: 'hns', e: 1 }, { d: 'hns' }],
+        [{ b: true, c: 3 }, {}], // Property `b` and `c` don't even exist on `B`.
+        [{ d: 'string value', e: 'no validation here' }, { d: 'string value' }],
     ],
 });
 
-type IntersectionTestType = The<typeof IntersectionTestType>;
-const IntersectionTestType = intersection('IntersectionTestType', [TestType, PartialTestType]);
+type IntersectAB = The<typeof IntersectAB>;
+const IntersectAB = intersection('IntersectAB', [A, B]);
 testTypeImpl({
-    name: `Pick<IntersectionTestType, 'a' | 'd'>`,
-    type: pick(IntersectionTestType, ['a', 'd']),
+    name: `Pick<IntersectAB, 'a' | 'd'>`,
+    type: pick(IntersectAB, ['a', 'd']),
     basicType: 'object',
-    validValues: [{ a: 'blah', d: 'dah' }, { a: 'blah2' }, { a: 'hts', d: '' }],
+    validValues: [{ a: 'string a', d: 'string d' }, { a: 'string a' }, { a: 'string a', d: '' }],
     invalidValues: [
-        [{ d: 'dah' }, `error in [Pick<IntersectionTestType, 'a' | 'd'>]: missing property <a> [string], got: { d: "dah" }`],
-        [{ a: 'blah2', d: undefined }, `error in [Pick<IntersectionTestType, 'a' | 'd'>] at <d>: expected a string, got an undefined`],
+        [{ d: 'string d' }, `error in [Pick<IntersectAB, 'a' | 'd'>]: missing property <a> [string], got: { d: "string d" }`],
+        [{ a: 'string a', d: undefined }, `error in [Pick<IntersectAB, 'a' | 'd'>] at <d>: expected a string, got an undefined`],
     ],
     validConversions: [
         [
-            { a: 'drop', b: true, c: 2, d: 'tnsh', e: 2 },
-            { a: 'drop', d: 'tnsh' },
+            { a: 'string a', b: 'not a bool', c: 2, d: 'string d', e: false },
+            { a: 'string a', d: 'string d' },
         ],
-        [{ a: 'tns' }, { a: 'tns' }],
     ],
 });
 
-type Overlap = The<typeof Overlap>;
-const Overlap = object('Overlap', {
+/** Property `a` and `b` overlapping in name and type with A, but `d` doesn't exist in A. */
+type C = The<typeof C>;
+const C = object('C', {
     a: string,
-    b: boolean,
+    b: number,
     d: boolean,
 });
-type UnionTestType = The<typeof UnionTestType>;
-const UnionTestType = union('UnionTestType', [TestType, Overlap]);
-// const pickunion = pick(UnionTestType, ['a']);
+type PickedUnionAC = The<typeof PickedUnionAC>;
+const PickedUnionAC = pick(union('UnionAC', [A, C]), ['a', 'd']);
 testTypeImpl({
-    name: `Pick<UnionTestType, 'a'>`,
-    type: pick(UnionTestType, ['a']),
+    name: `Pick<UnionAC, 'a' | 'd'>`,
+    type: PickedUnionAC,
     basicType: 'object',
-    validValues: [{ a: 'str' }, { a: '' }],
+    validValues: [{ a: 'string a', d: true }, { a: 'string a' }],
     invalidValues: [
-        [{}, `error in [Pick<UnionTestType, 'a'>]: missing property <a> [string | string], got: {}`],
-        [{ a: true }, `error in [Pick<UnionTestType, 'a'>] at <a>: expected a string, got a boolean (true)`],
+        [
+            { d: true },
+            [
+                `error in [Pick<UnionAC, 'a' | 'd'>]: failed every element in union:`,
+                `(got: { d: true })`,
+                `  • error in [Pick<C, 'a' | 'd'>]: missing property <a> [string]`,
+                `  • error in [Pick<A, 'a'>]: missing property <a> [string]`,
+            ],
+        ],
     ],
     validConversions: [
-        [{ a: 'Overlap', b: true }, { a: 'Overlap' }],
-        [{ a: 'TestType', b: false, c: 12 }, { a: 'TestType' }],
+        [{ a: 'string a', b: true }, { a: 'string a' }],
+        [
+            { a: 'string a2', d: true, c: undefined },
+            { a: 'string a2', d: true },
+        ],
     ],
 });
+assignableTo<PickedUnionAC>({ a: 'string' });
+assignableTo<PickedUnionAC>({ a: 'string', d: true });
+// @ts-expect-error because no union element accepts just { d: boolean }
+assignableTo<PickedUnionAC>({ d: true });
 
-type NastyType = The<typeof NastyType>;
-const NastyType = object('NastyType', {
-    union: UnionTestType,
-    intersection: pick(IntersectionTestType, ['a', 'd']),
-    inlineUnion: literal('stringliteral').or(literal(12)),
-    nestedOptionals: object({
-        req: string,
-    }).withOptional({
-        opt: string,
-    }),
-    arr: array(number).withConstraint('nonempty', i => i.length > 0),
-}).withOptional({
-    optionalUnion: pick(UnionTestType, ['a']).or(pick(IntersectionTestType, ['c', 'd'])),
-    optionalIntersection: pick(IntersectionTestType, ['a', 'd']),
-});
+/**  { a: string; b: boolean; }
+ * | { a: string; b: number; }
+ * | { a: number; b: string; } */
+type DiscriminatedUnion = The<typeof DiscriminatedUnion>;
+const DiscriminatedUnion = pick('DiscriminatedUnion', A.or(C).or(object({ a: number, b: string, c: number })), ['a', 'b']);
 testTypeImpl({
-    type: pick(NastyType, ['inlineUnion', 'optionalIntersection']),
-    name: `Pick<NastyType, 'inlineUnion' | 'optionalIntersection'>`,
+    name: `DiscriminatedUnion`,
+    type: DiscriminatedUnion,
     basicType: 'object',
     validValues: [
-        { inlineUnion: 'stringliteral', optionalIntersection: { a: 'reqstring', d: 'optionalstring' } },
-        { inlineUnion: 12, optionalIntersection: { a: 'reqstring' } },
-        { inlineUnion: 'stringliteral' },
+        { a: 'string', b: true },
+        { a: 'string', b: 2 },
+        { a: 3, b: 'string' },
     ],
-    invalidValues: [
+    invalidConversions: [
         [
-            { inlineUnion: 13, optionalIntersection: { a: 'reqstring', d: 'optionalstring' } },
+            { a: 'string 1', b: 'string 2', c: null },
             [
-                `error in [Pick<NastyType, 'inlineUnion' | 'optionalIntersection'>] at <inlineUnion>: in union element [12]: expected the literal 12, got: 13`,
-                `  • disregarded 1 union-subtype that does not accept a number`,
+                `error in [DiscriminatedUnion]: failed every element in union:`,
+                `(got: { a: "string 1", b: "string 2", c: null })`,
+                `  • error in [Pick<A | C, 'a' | 'b'>] at <b>: expected a boolean or a number, got a string ("string 2")`,
+                `  • error in [Pick<{ a: number, b: string, c: number }, 'a' | 'b'>] at <a>: expected a number, got a string ("string 1")`,
             ],
-        ],
-        [
-            { optionalIntersection: { a: 'reqstring', d: 'optionalstring' } },
-            `error in [Pick<NastyType, 'inlineUnion' | 'optionalIntersection'>]: missing property <inlineUnion> ["stringliteral" | 12], got: { optionalIntersection: { a: "reqstring", d: " .. " } }`,
-        ],
-        [
-            { inlineUnion: 12, optionalIntersection: { a: 'reqstring', d: undefined } },
-            `error in [Pick<NastyType, 'inlineUnion' | 'optionalIntersection'>] at <optionalIntersection.d>: expected a string, got an undefined`,
-        ],
-        [
-            { inlineUnion: 12, optionalIntersection: {} },
-            `error in [Pick<NastyType, 'inlineUnion' | 'optionalIntersection'>] at <optionalIntersection>: missing property <a> [string], got: {}`,
         ],
     ],
 });
+assignableTo<DiscriminatedUnion>({ a: 'string', b: true });
+assignableTo<DiscriminatedUnion>({ a: 'string', b: 3 });
+assignableTo<DiscriminatedUnion>({ a: 3, b: 'true' });
+// @ts-expect-error because no union element accepts string, string
+assignableTo<DiscriminatedUnion>({ a: 'string', b: 'true' });
+// @ts-expect-error because no union element accepts number, bool
+assignableTo<DiscriminatedUnion>({ a: 3, b: true });
+// @ts-expect-error because no union element accepts number, number
+assignableTo<DiscriminatedUnion>({ a: 3, b: 3 });
 
-const example1 = createExample(NastyType, 1);
-const example2 = createExample(NastyType, 2);
-
+// { a: 'string' } | { b: true }, picking 'b' => { b: true } is the only valid variant for the picked key.
+type PickUniqueVariantProp = The<typeof PickUniqueVariantProp>;
+const PickUniqueVariantProp = pick(object({ a: literal('string') }).or(object({ b: literal(true) })), ['b']);
 testTypeImpl({
-    type: pick(NastyType, ['optionalUnion', 'arr']),
-    name: `Pick<NastyType, 'optionalUnion' | 'arr'>`,
+    name: `Pick<{ a: "string" } | { b: true }, 'b'>`,
+    type: PickUniqueVariantProp,
     basicType: 'object',
-    validValues: [
-        { optionalUnion: { a: 'pickedUnion' }, arr: [1] },
-        { optionalUnion: { c: 12, d: 'pickedIntersection' }, arr: [12, 3] },
-        { optionalUnion: { c: 1 }, arr: [12, 3] },
-        { arr: [12, 3] },
-    ],
-    invalidValues: [
-        [
-            { optionalUnion: 13, arr: [] },
-            [
-                `errors in [Pick<NastyType, 'optionalUnion' | 'arr'>]:`,
-                `- at <optionalUnion>: expected an object, got a number (13)`,
-                `- at <arr>: expected a [nonempty], got: []`,
-            ],
-        ],
-        [
-            { optionalUnion: { a: 'from UnionTestType', c: 'from IntersectionTestType', d: 'from IntersectionTestType' }, arr: [12] },
-            `error`,
-        ],
-    ],
-    validConversions: [[example1, { optionalUnion: example1.optionalUnion, arr: example1.arr }]],
+    validValues: [{ b: true }],
+    validConversions: [[{ a: 'string', b: true, c: null }, { b: true }]],
 });
 
 describe('Other', () => {
-    test('narrowing', () => {
-        expect(pick(NastyType, ['optionalUnion', 'arr'])(example1)).toEqual({
-            optionalUnion: {
-                a: 'xxxxxxxxxxxxxxxx',
-            },
-            arr: example1.arr,
-        });
-        expect(pick(NastyType, ['optionalUnion', 'arr'])(example2)).toEqual({
-            optionalUnion: {
-                c: 0.14,
-                d: 'xxxxxxxxxxxxxxx',
-            },
-            arr: example2.arr,
-        });
+    test('Invalid basic types for input', () => {
+        // When parsing input on a valid pick type
+        expect(() => DiscriminatedUnion(1)).toThrow('error in [DiscriminatedUnion]: expected an object, got a number');
+        // When constructing a new pick type from a union
+        expect(() => pick(array(A).or(B), ['d'])).toThrow(`Can only pick elements of unions with 'object' as basic type.`);
     });
-    test('pickProperties', () => {
-        expect(pickProperties(TestType.props, ['a', 'b'])).toEqual({
-            a: string,
-            b: boolean,
+    describe('Union variant validation helper functions', () => {
+        const types = checkOneOrMore([
+            object({ a: literal('a'), b: literal('b'), c: literal('c') }),
+            object({ b: literal('b'), c: literal('c'), d: literal('d') }),
+            object({ c: literal('c'), d: literal('d'), e: literal('e') }),
+        ]);
+        test.each`
+            pickedKeys    | narrowedKeys                       | isValidPick
+            ${['a']}      | ${[['a'], [], []]}                 | ${true}
+            ${['b']}      | ${[['b'], ['b'], []]}              | ${true}
+            ${['c']}      | ${[['c'], ['c'], ['c']]}           | ${true}
+            ${['a', 'b']} | ${[['a', 'b'], ['b'], []]}         | ${true}
+            ${['c', 'd']} | ${[['c'], ['c', 'd'], ['c', 'd']]} | ${true}
+            ${['a', 'd']} | ${[['a'], ['d'], ['d']]}           | ${false}
+            ${['a', 'e']} | ${[['a'], [], ['e']]}              | ${false}
+        `('Picking $pickedKeys should be valid: $isValidPick', ({ pickedKeys, narrowedKeys, isValidPick }) => {
+            expect(narrowPickedKeys(pickedKeys, types)).toEqual(narrowedKeys);
+            expect(validPick(pickedKeys, narrowedKeys)).toBe(isValidPick);
         });
-    });
-    test('pickPropertiesInfo', () => {
-        expect(pickPropertiesInfo(TestType.propsInfo, ['a', 'b'])).toEqual({
-            a: {
-                partial: false,
-                type: string,
-            },
-            b: {
-                partial: false,
-                type: boolean,
-            },
-        });
-        expect(pickPropertiesInfo(PartialTestType.propsInfo, ['d'])).toEqual({
-            d: {
-                partial: true,
-                type: string,
-            },
+        test('Impossible union variant pick construction should throw', () => {
+            expect(() => pick('Unpickable', union(types), ['a', 'd'])).toThrow('Selected keys describe impossible union variant.');
         });
     });
 });
--- src/types/pick.ts
+++ src/types/pick.ts
@@ -83,10 +83,34 @@ export function pick<TypeImpl extends PickableImpl, Keys extends PickableKeys<Ty
     const [name, baseType, keys] = decodeOptionalName(args);
     if (baseType instanceof UnionType) {
         const unionType: UnionType<OneOrMore<BaseObjectLikeTypeImpl<TypeOf<TypeImpl>>>, TypeOf<TypeImpl>> = baseType;
-        return union(checkOneOrMore(unionType.types.map(type => new PickType(type, keys)))) as unknown as ObjectType<
-            DistributedPick<TypeOf<TypeImpl>, Keys>
-        >;
+        if (!(unionType.basicType === 'object')) {
+            throw new Error(`Can only pick elements of unions with 'object' as basic type.`);
+        }
+        const narrowedKeys = narrowPickedKeys(keys, unionType.types);
+        if (!validPick(keys, narrowedKeys)) {
+            throw new Error('Selected keys describe impossible union variant.');
+        }
+
+        const innerTypes = unionType.types
+            .map((innerType, idx) => {
+                const specificKeys = narrowedKeys[idx] ?? [];
+                const keyCount = specificKeys.length;
+                // Only build the pick union elements if at least one of their keys has been picked.
+                // Also temporarily store the key count in a tuple to sort.
+                return [keyCount, keyCount ? new PickType(innerType, checkOneOrMore(specificKeys)) : undefined] as const;
+            })
+            // Sorting is necessary so the union type will internally match with a maximum subtype. This is so that types don't accidentally
+            // get narrowed too much during parsing and stringifying.
+            .sort(([aKeys, _1], [bKeys, _2]) => bKeys - aKeys) // Reverse sort: from most keys to least keys.
+            .flatMap(([_, innerType]) => innerType ?? []);
+
+        return union(
+            // Manually rename the type, because it's confusing if your union elements are suddenly in a different order.
+            name ?? `Pick<${baseType.name}, ${keys.map(key => `'${key}'`).join(' | ')}>`,
+            checkOneOrMore(innerTypes),
+        ) as unknown as ObjectType<DistributedPick<TypeOf<TypeImpl>, Keys>>;
     }
+
     return createType(new PickType(baseType, keys, name));
 }
 
@@ -96,3 +120,20 @@ export const pickPropertiesInfo = (propsInfo: PropertiesInfo, keys: OneOrMore<ke
             .map(property => [property, propsInfo[property]])
             .filter((v): v is [string, PropertiesInfo[keyof PropertiesInfo]] => v[1] !== undefined),
     );
+
+/** Return the intersection of picked keys with each of the property keys of the inner types. */
+export const narrowPickedKeys = <TypeImpl extends PickableImpl, Keys extends PickableKeys<TypeOf<TypeImpl>>>(
+    pickedKeys: OneOrMore<Keys>,
+    innerTypes: OneOrMore<TypeImpl>,
+): OneOrMore<Keys[]> =>
+    innerTypes
+        .map(type => Object.keys(type.props))
+        .map(innerKeys => pickedKeys.filter(pickedKey => innerKeys.find(innerKey => pickedKey === innerKey))) as OneOrMore<Keys[]>;
+
+/**
+ * Check if the intersection of all the non-empty narrowed keys is not empty. If it is, it means the picked keys describe an impossible
+ * union variant. Empty narrowed keys are ignored, because they will not end up in the union at all.
+ */
+export const validPick = (pickedKeys: string[], innerTypeKeys: OneOrMore<string[]>): boolean =>
+    innerTypeKeys.reduce((intersect, keys) => intersect.filter(k1 => (keys.length ? keys.find(k2 => k1 === k2) : true)), pickedKeys)
+        .length > 0;
